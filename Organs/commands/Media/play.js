6b3a9a0ba8e8592d67e6bbbb51ac7ee11f88114e
retrogamer234 <101521533+retrogamer234@users.noreply.github.com>
2023-04-18 19:11:35 +0530

    Update play.js

--- Organs/commands/Media/play.js
+++ Organs/commands/Media/play.js
@@ -24,7 +24,7 @@ await client.sendMessage(m.from,{
     contextInfo:{
         externalAdReply:{
             title:anu.title,
-            body: "©Eternity-Team",
+            body: "Mitsu",
             thumbnail: await fetchBuffer(pl.meta.image),
             mediaType:2,
             mediaUrl:anu.url,
